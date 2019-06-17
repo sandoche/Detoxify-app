674b945962abaf9b371e0e100587f3850b325193
Sandoche Adittane <sandoche@protonmail.com>
2019-06-17 13:00:51 +0200

    :construction: The SW does not give error this time

--- controller/pwaController.js
+++ controller/pwaController.js
@@ -16,78 +16,109 @@ router.get('/:appId/manifest\.json', async function(req, res, next) {
 
 router.get('/:appId/service-worker\.js', function(req, res, next) {
   res.set('Content-Type', 'application/javascript');
-  res.send(`/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = '${req.params.appId}-precache-v1';
-const RUNTIME = 'runtime';
-
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  'index.html',
-  './'
-];
-
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+  res.send(`// Set a name for the current cache
+  var cacheName = '${req.params.appId}-v1'; 
+  
+  // Default files to always cache
+  var cacheFiles = [
+    '/pwa/:appId',
+    '/pwa/:appId/'
+  ]
+  
+  
+  self.addEventListener('install', function(e) {
+      console.log('[ServiceWorker] Installed');
+  
+      // e.waitUntil Delays the event until the Promise is resolved
+      e.waitUntil(
+  
+        // Open the cache
+        caches.open(cacheName).then(function(cache) {
+  
+          // Add all the default files to the cache
+        console.log('[ServiceWorker] Caching cacheFiles');
+        return cache.addAll(cacheFiles);
+        })
+    ); // end e.waitUntil
+  });
+  
+  
+  self.addEventListener('activate', function(e) {
+      console.log('[ServiceWorker] Activated');
+  
+      e.waitUntil(
+  
+        // Get all the cache keys (cacheName)
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(cacheNames.map(function(thisCacheName) {
+  
+          // If a cached item is saved under a previous cacheName
+          if (thisCacheName !== cacheName) {
+  
+            // Delete that cached file
+            console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+            return caches.delete(thisCacheName);
+          }
+        }));
       })
-    );
-  }
-});`)
+    ); // end e.waitUntil
+  
+  });
+  
+  
+  self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+  
+    // e.respondWidth Responds to the fetch event
+    e.respondWith(
+  
+      // Check in cache for the request being made
+      caches.match(e.request)
+  
+  
+        .then(function(response) {
+  
+          // If the request is in the cache
+          if ( response ) {
+            console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+            // Return the cached version
+            return response;
+          }
+  
+          // If the request is NOT in the cache, fetch and cache
+  
+          var requestClone = e.request.clone();
+          return fetch(requestClone)
+            .then(function(response) {
+  
+              if ( !response ) {
+                console.log("[ServiceWorker] No response from fetch ")
+                return response;
+              }
+  
+              var responseClone = response.clone();
+  
+              //  Open the cache
+              caches.open(cacheName).then(function(cache) {
+  
+                // Put the fetched response in the cache
+                cache.put(e.request, responseClone);
+                console.log('[ServiceWorker] New Data Cached', e.request.url);
+  
+                // Return the response
+                return response;
+        
+                  }); // end caches.open
+  
+            })
+            .catch(function(err) {
+              console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+            });
+  
+  
+        }) // end caches.match(e.request)
+    ); // end e.respondWith
+  });`)
 });
 
 module.exports = router;
