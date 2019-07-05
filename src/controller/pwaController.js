13787cafcc651a3379cbc36719c1483d1487d06f
Sandoche Adittane <sandoche@protonmail.com>
2019-07-05 17:33:54 +0200

    :wrench: Cache version updated

--- src/controller/pwaController.js
+++ src/controller/pwaController.js
@@ -33,7 +33,7 @@ router.get('/:appId/service-worker\.js', function(req, res, next) {
  // Names of the two caches used in this version of the service worker.
  // Change to v2, etc. when you update any of the local resources, which will
  // in turn trigger the install event again.
- const PRECACHE = '${req.params.appId}-precache-v2';
+ const PRECACHE = '${req.params.appId}-precache-v3';
  const RUNTIME = 'runtime';
  
  // A list of local resources we always want to be cached.
