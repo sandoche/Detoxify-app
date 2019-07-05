76f32761244c700ea551876c474bcaef0e76ddf8
Sandoche Adittane <sandoche@protonmail.com>
2019-07-05 18:04:50 +0200

    :sparkles: 404 Error page designed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
   title: 'Detoxify.app',
+  short_description: 'Digital detox made easy',
   description: 'Generate a fake app to replace any addictive app 📱🙅',
   long_description: 'Use AppDetox to help you detox from any addictive app',
   keywords: 'digital detox, detox, pwa',
